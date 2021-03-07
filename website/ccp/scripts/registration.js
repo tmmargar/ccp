8a8e3e28b66e0db589c517fc29e1388f37ae4df9
Todd Margarita <todd@toddandpatti.com>
2021-03-07 18:30:52 -0500

    Remove unused javascript files and remove setFormValues

--- website/ccp/scripts/registration.js
+++ website/ccp/scripts/registration.js
@@ -3,15 +3,11 @@ $(document).ready(function() {
   input.validateLength($("#food"), 1, true)
 });
 $(document).on("keyup paste", "#food", function(event) {
-  if ("" == input.validateLength($(this), 1, true, "error")) {
-    $("#register").prop("disabled", false);
-  } else {
-    $("#register").prop("disabled", true);
-  }
+  $("#register").prop("disabled", !("" == input.validateLength($(this), 1, true, "error")));
 });
 $(document).on("click", "#register", function(event) {
-  input.setFormValues(["mode"], ["savecreate"]);
+  $("#mode").val("savecreate");
 });
 $(document).on("click", "#unregister", function(event) {
-  input.setFormValues(["mode"], ["savemodify"]);
+  $("#mode").val("savemodify");
 });
