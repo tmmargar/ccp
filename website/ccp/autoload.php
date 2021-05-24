a3351ff3daea04c0be6784e6bdd26fc551c0a79a
Todd Margarita <todd@toddandpatti.com>
2021-05-23 20:32:34 -0400

    Change demo to new so path is valid

--- website/ccp/autoload.php
+++ website/ccp/autoload.php
@@ -8,7 +8,7 @@ namespace ccp;
  */
   spl_autoload_register(function($class_name) {
     $class_name = str_replace(search: "\\", replace: "/", subject: $class_name);
-    $rootDir = $_SERVER['HTTP_HOST'] == "www.chipchairprayer.com" ? "/home/chipch5/public_html/demo/" : "";
+    $rootDir = $_SERVER['HTTP_HOST'] == "www.chipchairprayer.com" ? "/home/chipch5/public_html/new/" : "";
     $dirs = array($rootDir, $rootDir . "classes/", $rootDir . "classes/test/", $rootDir . "classes/common/PHPMailer/", $rootDir . "classes/common/Psr/", $rootDir . "classes/common/smarty/", $rootDir . "classes/common/smarty/libs/", $rootDir . "classes/utility/", $rootDir . "classes/utility/test/");
     foreach($dirs as $dir) {
 //        echo "<br>namespace CCP -->" . $class_name . " -> " . $dir . str_replace("ccp/", "", $class_name) . ".php";
