4fc5610af9cca1abb83887b6b1e113321a1d919f
Todd Margarita <tmmargar@gmail.com>
2021-12-30 21:53:09 -0500

    Fix date logic for registration open

--- website/ccp/scripts/registration.js
+++ website/ccp/scripts/registration.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   input.validateLength($("#food"), 1, true)
 });
 $(document).on("keyup paste", "#food", function(event) {
-  $("#register").prop("disabled", !("" == input.validateLength($(this), 1, true, "error")));
+  $("#register").prop("disabled", !("" == input.validateLength($(this), 1, true)));
 });
 $(document).on("click", "#register", function(event) {
   $("#mode").val("savecreate");
