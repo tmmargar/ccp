789c8b08e715a7f69e29480ef8e9b7a27b1465c8
Todd Margarita <todd@toddandpatti.com>
2022-01-01 19:05:21 -0500

    Add 7 days

--- website/ccp/autoReminder.php
+++ website/ccp/autoReminder.php
@@ -28,7 +28,26 @@ if (count($resultList2) == 0) {
       $locationUser = $tournament->getLocation()->getUser();
       $tournamentAddress = $locationUser->getAddress();
       $email = new Email(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), fromName: array(Constant::$NAME_STAFF), fromEmail: array(Constant::EMAIL_STAFF()), toName: array($user->getName()), toEmail: array($user->getEmail()), ccName: null, ccEmail: null, bccName: null, bccEmail: null, subject: null, body: null);
-      $emailAddress = new Address(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), id: null, address: $tournamentAddress->getAddress(), city: $tournamentAddress->getCity(), state: $tournamentAddress->getState(), zip: $tournamentAddress->getZip(), phone: null);
+      $emailAddress = new Address(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), id: null, address: $tournamentAddress->getAddress(), city: $tournamentAddress->getCity(), state: $tournamentAddress->getState(), zip: $tournamentAddress->getZip());
+      $output .= isset($mode) ? "  aryMessages.push(\"" . $email->sendReminderEmail(address: $emailAddress, tournament: $tournament, waitListCount: 0) . "\");\n" : "\r";
+    }
+  }
+}
+$days = 7;
+$params = array($days);
+$resultList = $databaseResult->getUsersForEmailNotifications(params: $params);
+$resultList2 = $databaseResult->getTournamentsForEmailNotifications(params: $params);
+if (count($resultList2) == 0) {
+  $output .= isset($mode) ? "  aryMessages.push(\"No tournaments needing auto email notification for registration open (" . $days . " days before)\");\n" : "\r";
+} else {
+  foreach ($resultList as $user) {
+    foreach ($resultList2 as $tournament) {
+      $dt = new \DateTime();
+      // echo "<br>" . $user->getName() . " -- " . $dt->getTimestamp();
+      $locationUser = $tournament->getLocation()->getUser();
+      $tournamentAddress = $locationUser->getAddress();
+      $email = new Email(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), fromName: array(Constant::$NAME_STAFF), fromEmail: array(Constant::EMAIL_STAFF()), toName: array($user->getName()), toEmail: array($user->getEmail()), ccName: null, ccEmail: null, bccName: null, bccEmail: null, subject: null, body: null);
+      $emailAddress = new Address(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), id: null, address: $tournamentAddress->getAddress(), city: $tournamentAddress->getCity(), state: $tournamentAddress->getState(), zip: $tournamentAddress->getZip());
       $output .= isset($mode) ? "  aryMessages.push(\"" . $email->sendReminderEmail(address: $emailAddress, tournament: $tournament, waitListCount: 0) . "\");\n" : "\r";
     }
   }
@@ -45,7 +64,7 @@ if (count($resultList2) == 0) {
       $locationUser = $tournament->getLocation()->getUser();
       $tournamentAddress = $locationUser->getAddress();
       $email = new Email(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), fromName: array(Constant::$NAME_STAFF), fromEmail: array(Constant::EMAIL_STAFF()), toName: array($user->getName()), toEmail: array($user->getEmail()), ccName: null, ccEmail: null, bccName: null, bccEmail: null, subject: null, body: null);
-      $emailAddress = new Address(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), id: null, address: $tournamentAddress->getAddress(), city: $tournamentAddress->getCity(), state: $tournamentAddress->getState(), zip: $tournamentAddress->getZip(), phone: null);
+      $emailAddress = new Address(debug: SessionUtility::getValue(SessionUtility::$OBJECT_NAME_DEBUG), id: null, address: $tournamentAddress->getAddress(), city: $tournamentAddress->getCity(), state: $tournamentAddress->getState(), zip: $tournamentAddress->getZip());
       $output .= isset($mode) ? "  aryMessages.push(\"" . $email->sendReminderEmail(address: $emailAddress, tournament: $tournament, waitListCount: 0) . "\");\n" : "\r";
     }
   }
