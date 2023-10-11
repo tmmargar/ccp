454299bb7b34623fbdd151bbc1faca265b1268f3
N082832 <N082832@CUNS40Q223-023.americas.ad.nsk.com>
2023-10-10 22:01:36 -0400

    Fix navigation indentation

--- website/ccp/classes/model/HtmlMenu.php
+++ website/ccp/classes/model/HtmlMenu.php
@@ -4,41 +4,41 @@ namespace ccp\classes\model;
 use ccp\classes\utility\SessionUtility;
 class HtmlMenu extends HtmlBase {
   public function __construct(protected bool $debug, protected string|int|null $id, protected array|null $items, protected string|null $text) {
-    parent::__construct(null, null, $debug, $id, 0, null);
+    parent::__construct(accessKey: null, class: null, debug: $debug, id: $id, tabIndex: 0, title: null);
   }
   public function getHtmlRoot() {
-    $output = str_repeat(" ", 1) . "<nav id=\"main-nav\">\n";
-    $output .= str_repeat(" ", 2) . "<input type=\"checkbox\" id=\"menu\" name=\"menu\" class=\"m-menu__checkbox\">\n";
-    $output .= str_repeat(" ", 2) . "<label class=\"m-menu__toggle\" for=\"menu\">\n";
-    $output .= str_repeat(" ", 3) . "<svg width=\"35\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" stroke=\"#fff\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
-    $output .= str_repeat(" ", 4) . "<line x1=\"3\" y1=\"6\" x2=\"21\" y2=\"6\"></line>\n";
-    $output .= str_repeat(" ", 4) . "<line x1=\"3\" y1=\"12\" x2=\"21\" y2=\"12\"></line>\n";
-    $output .= str_repeat(" ", 4) . "<line x1=\"3\" y1=\"18\" x2=\"21\" y2=\"18\"></line>\n";
-    $output .= str_repeat(" ", 3) . "</svg>\n";
-    $output .= str_repeat(" ", 2) . "</label>\n";
-    $output .= str_repeat(" ", 2) . "<label class=\"m-menu__overlay\" for=\"menu\"></label>\n";
-    $output .= str_repeat(" ", 2) . "<div class=\"m-menu\">\n";
-    $output .= str_repeat(" ", 3) . "<div class=\"m-menu__header\">\n";
-    $output .= str_repeat(" ", 4) . "<label class=\"m-menu__toggle\" for=\"menu\">\n";
-    $output .= str_repeat(" ", 5) . "<svg width=\"35\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" stroke=\"#000000\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
-    $output .= str_repeat(" ", 6) . "<line x1=\"18\" y1=\"6\" x2=\"6\" y2=\"18\"></line>\n";
-    $output .= str_repeat(" ", 6) . "<line x1=\"6\" y1=\"6\" x2=\"18\" y2=\"18\"></line>\n";
-    $output .= str_repeat(" ", 5) . "</svg>\n";
-    $output .= str_repeat(" ", 4) . "</label>\n";
-    $output .= str_repeat(" ", 4) . "<span>MENU</span>\n";
-    $output .= str_repeat(" ", 3) . "</div>\n";
-    $output .= str_repeat(" ", 3) . "<ul>\n";
-    $output .= $this->getHtml(true, 3);
-    $output .= str_repeat(" ", 3) . "</ul>\n";
-    $output .= str_repeat(" ", 2) . "</div>\n";
-//     $output .= str_repeat(" ", 2) . "<span style=\"color: white; font-size: 1.25em;\">\n";
-//     $output .= str_repeat(" ", 3) . "<svg width=\"35\" height=\"35\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"#ffffff\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
-//     $output .= str_repeat(" ", 4) . "<path d=\"M19 12H6M12 5l-7 7 7 7\"/>\n";
-//     $output .= str_repeat(" ", 3) . "</svg>\n";
-//     $output .= str_repeat(" ", 2) . "</span>\n";
-//     $output .= str_repeat(" ", 2) . "<span style=\"color: white; font-size: 1.25em;\">Menu</span>\n";
-    $output .= str_repeat(" ", 2) . "<span style=\"color: white; font-size: 1em;\">\n";
-    if ("" == SessionUtility::getValue(SessionUtility::$OBJECT_NAME_START_DATE)) {
+    $output = str_repeat(string: " ", times: 1) . "<nav id=\"main-nav\">\n";
+    $output .= str_repeat(string: " ", times: 2) . "<input type=\"checkbox\" id=\"menu\" name=\"menu\" class=\"m-menu__checkbox\">\n";
+    $output .= str_repeat(string: " ", times: 2) . "<label class=\"m-menu__toggle\" for=\"menu\">\n";
+    $output .= str_repeat(string: " ", times: 3) . "<svg width=\"35\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" stroke=\"#fff\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
+    $output .= str_repeat(string: " ", times: 4) . "<line x1=\"3\" y1=\"6\" x2=\"21\" y2=\"6\"></line>\n";
+    $output .= str_repeat(string: " ", times: 4) . "<line x1=\"3\" y1=\"12\" x2=\"21\" y2=\"12\"></line>\n";
+    $output .= str_repeat(string: " ", times: 4) . "<line x1=\"3\" y1=\"18\" x2=\"21\" y2=\"18\"></line>\n";
+    $output .= str_repeat(string: " ", times: 3) . "</svg>\n";
+    $output .= str_repeat(string: " ", times: 2) . "</label>\n";
+    $output .= str_repeat(string: " ", times: 2) . "<label class=\"m-menu__overlay\" for=\"menu\"></label>\n";
+    $output .= str_repeat(string: " ", times: 2) . "<div class=\"m-menu\">\n";
+    $output .= str_repeat(string: " ", times: 3) . "<div class=\"m-menu__header\">\n";
+    $output .= str_repeat(string: " ", times: 4) . "<label class=\"m-menu__toggle\" for=\"menu\">\n";
+    $output .= str_repeat(string: " ", times: 5) . "<svg width=\"35\" height=\"20\" viewBox=\"0 0 20 20\" fill=\"none\" stroke=\"#000000\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
+    $output .= str_repeat(string: " ", times: 6) . "<line x1=\"18\" y1=\"6\" x2=\"6\" y2=\"18\"></line>\n";
+    $output .= str_repeat(string: " ", times: 6) . "<line x1=\"6\" y1=\"6\" x2=\"18\" y2=\"18\"></line>\n";
+    $output .= str_repeat(string: " ", times: 5) . "</svg>\n";
+    $output .= str_repeat(string: " ", times: 4) . "</label>\n";
+    $output .= str_repeat(string: " ", times: 4) . "<span>MENU</span>\n";
+    $output .= str_repeat(string: " ", times: 3) . "</div>\n";
+    $output .= str_repeat(string: " ", times: 3) . "<ul>\n";
+    $output .= $this->getHtml(parent: true, counter: 3);
+    $output .= str_repeat(string: " ", times: 3) . "</ul>\n";
+    $output .= str_repeat(string: " ", times: 2) . "</div>\n";
+    //     $output .= str_repeat(string: " ", times: 2) . "<span style=\"color: white; font-size: 1.25em;\">\n";
+//     $output .= str_repeat(string: " ", times: 3) . "<svg width=\"35\" height=\"35\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"#ffffff\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
+//     $output .= str_repeat(string: " ", times: 4) . "<path d=\"M19 12H6M12 5l-7 7 7 7\"/>\n";
+//     $output .= str_repeat(string: " ", times: 3) . "</svg>\n";
+//     $output .= str_repeat(string: " ", times: 2) . "</span>\n";
+//     $output .= str_repeat(string: " ", times: 2) . "<span style=\"color: white; font-size: 1.25em;\">Menu</span>\n";
+    $output .= str_repeat(string: " ", times: 2) . "<span style=\"color: white; font-size: 1em;\">\n";
+    if ("" == SessionUtility::getValue(name: SessionUtility::$OBJECT_NAME_START_DATE)) {
       $output .= "None";
     } else {
       $output .= SessionUtility::getValue(name: SessionUtility::$OBJECT_NAME_START_DATE)->getDisplayFormat();
@@ -47,12 +47,12 @@ class HtmlMenu extends HtmlBase {
       $output .= "&nbsp;(";
       $output .= SessionUtility::getValue(name: SessionUtility::$OBJECT_NAME_CHAMPIONSHIP_QUALIFY) . " to qualify)";
     }
-    $output .= str_repeat(" ", 2) . "</span>\n";
-    $output .= str_repeat(" ", 1) . "</nav>\n";
+    $output .= str_repeat(string: " ", times: 2) . "</span>\n";
+    $output .= str_repeat(string: " ", times: 1) . "</nav>\n";
     
     /*$output .= " <div id=\"seasonInfo\">\n";
     $output .= "<strong>Season Info</strong>\n<br />\n";
-    if ("" == SessionUtility::getValue(SessionUtility::$OBJECT_NAME_START_DATE)) {
+    if ("" == SessionUtility::getValue(name: SessionUtility::$OBJECT_NAME_START_DATE)) {
       $output .= "None";
     } else {
       $output .= SessionUtility::getValue(name: SessionUtility::$OBJECT_NAME_START_DATE)->getDisplayFormat();
@@ -68,26 +68,26 @@ class HtmlMenu extends HtmlBase {
   public function getHtml(bool $parent, int $counter) {
     $output = "";
     foreach ($this->items as $item) { // $menu is HtmlLink or HtmlMenu object
-      if (get_class($item) == "ccp\classes\model\HtmlLink") {
-        $output .= str_repeat(" ", $counter + 1) . "<li><label>" . $item->getHtml() . "</label></li>\n";
+      if (get_class(object: $item) == "ccp\classes\model\HtmlLink") {
+        $output .= str_repeat(string: " ", times: $counter + 1) . "<li><span>" . $item->getHtml() . "</span></li>\n";
       } else {
-        $output .= str_repeat(" ", $counter + 1) . "<li>\n";
-        $output .= str_repeat(" ", $counter + 2) . "<label class=\"a-label__chevron\" for=\"item-" . $item->getText() . "\">" . $item->getText() . "</label>\n";
-        $output .= str_repeat(" ", $counter + 2) . "<input type=\"checkbox\" id=\"item-" . $item->getText() . "\" name=\"item-" . $item->getText() . "\" class=\"m-menu__checkbox\">\n";
-        $output .= str_repeat(" ", $counter + 2) . "<div class=\"m-menu\">\n";
-        $output .= str_repeat(" ", $counter + 3) . "<div class=\"m-menu__header\">\n";
-        $output .= str_repeat(" ", $counter + 4) . "<label class=\"m-menu__toggle\" for=\"item-" . $item->getText() . "\">\n";
-        $output .= str_repeat(" ", $counter + 5) . "<svg width=\"35\" height=\"20\" viewBox=\"0 0 15 22\" fill=\"none\" stroke=\"#000000\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
-        $output .= str_repeat(" ", $counter + 6) . "<path d=\"M19 12H6M12 5l-7 7 7 7\"/>\n";
-        $output .= str_repeat(" ", $counter + 5) . "</svg>\n";
-        $output .= str_repeat(" ", $counter + 4) . "</label>\n";
-        $output .= str_repeat(" ", $counter + 4) . "<span>" . $item->getText() . "</span>\n";
-        $output .= str_repeat(" ", $counter + 3) . "</div>\n";
-        $output .= str_repeat(" ", $counter + 3) . "<ul>\n";
-        $output .= $item->getHtml(false, $counter + 3);
-        $output .= str_repeat(" ", $counter + 3) . "</ul>\n";
-        $output .= str_repeat(" ", $counter + 2) . "</div>\n";
-        $output .= str_repeat(" ", $counter + 1) . "</li>\n";
+        $output .= str_repeat(string: " ", times: $counter + 1) . "<li>\n";
+        $output .= str_repeat(string: " ", times: $counter + 2) . "<label class=\"a-label__chevron\" for=\"item-" . $item->getText() . "\">" . $item->getText() . "</label>\n";
+        $output .= str_repeat(string: " ", times: $counter + 2) . "<input type=\"checkbox\" id=\"item-" . $item->getText() . "\" name=\"item-" . $item->getText() . "\" class=\"m-menu__checkbox\">\n";
+        $output .= str_repeat(string: " ", times: $counter + 2) . "<div class=\"m-menu\">\n";
+        $output .= str_repeat(string: " ", times: $counter + 3) . "<div class=\"m-menu__header\">\n";
+        $output .= str_repeat(string: " ", times: $counter + 4) . "<label class=\"m-menu__toggle\" for=\"item-" . $item->getText() . "\">\n";
+        $output .= str_repeat(string: " ", times: $counter + 5) . "<svg width=\"35\" height=\"20\" viewBox=\"0 0 15 22\" fill=\"none\" stroke=\"#000000\" stroke-width=\"2\" stroke-linecap=\"butt\" stroke-linejoin=\"arcs\">\n";
+        $output .= str_repeat(string: " ", times: $counter + 6) . "<path d=\"M19 12H6M12 5l-7 7 7 7\"/>\n";
+        $output .= str_repeat(string: " ", times: $counter + 5) . "</svg>\n";
+        $output .= str_repeat(string: " ", times: $counter + 4) . "</label>\n";
+        $output .= str_repeat(string: " ", times: $counter + 4) . "<span>" . $item->getText() . "</span>\n";
+        $output .= str_repeat(string: " ", times: $counter + 3) . "</div>\n";
+        $output .= str_repeat(string: " ", times: $counter + 3) . "<ul>\n";
+        $output .= $item->getHtml(parent: false, counter: $counter + 3);
+        $output .= str_repeat(string: " ", times: $counter + 3) . "</ul>\n";
+        $output .= str_repeat(string: " ", times: $counter + 2) . "</div>\n";
+        $output .= str_repeat(string: " ", times: $counter + 1) . "</li>\n";
       }
     }
     return $output;
@@ -107,7 +107,7 @@ class HtmlMenu extends HtmlBase {
   public function __toString() {
     $output = parent::__toString();
     $output .= ", items = ";
-    $output .= print_r($this->items, true);
+    $output .= print_r(value: $this->items, return: true);
     $output .= ", text = '";
     $output .= $this->text;
     $output .= "'";
